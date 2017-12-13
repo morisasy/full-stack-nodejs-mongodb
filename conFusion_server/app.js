db5a9fe5b6246ff11ad41de748bd83b3677ba6d3
morisasy@gmail.com <morisasy@gmail.com>
2017-12-13 16:34:31 +0200

    Basic Authentification

--- conFusion_server/app.js
+++ conFusion_server/app.js
@@ -57,6 +57,31 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
 
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
